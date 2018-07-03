a634f0b03bd31034d90ffd33f2391e183badd297
Troy Jaggard <troy@mymediamagnet.com>
2018-07-02 18:37:55 -0600

    refactor command tests

--- tests/Unit/ArtisanTest.php
+++ tests/Unit/ArtisanTest.php
@@ -8,31 +8,21 @@ use MultiTenantLaravel\Tests\Models\User;
 use MultiTenantLaravel\Tests\Models\Role;
 use MultiTenantLaravel\Tests\Models\Tenant;
 use Illuminate\Support\Facades\Artisan;
-use Symfony\Component\Console\Application as ConsoleApplication;
 
 class ArtisanTest extends TestCase
 {
+    public $command;
+
     public function testFakeUserCanBeCreatedWithNoTenants()
     {
         // In order to properly test our commands, we need to mock the ask()
         // and anticipate() methods but leave the rest of the class to behave as normal
-        $faker = \Faker\Factory::create();
-        $command = \Mockery::mock("\MultiTenantLaravel\App\Commands\CreateUser[ask, anticipate]", [$faker])->makePartial();
+        $this->mockCommand("\MultiTenantLaravel\App\Commands\CreateUser[ask, anticipate]");
 
-        $command->shouldReceive('ask')
-            ->once()
-            ->with('How many would you like to create?')
-            ->andReturn('1');
+        $this->asks('How many would you like to create?', '1');
+        $this->anticipates('Would you like to assign the user to a tenant?', ['Yes', 'No'], 'Yes', 'Yes');
 
-        $command->shouldReceive('anticipate')
-            ->once()
-            ->with('Would you like to assign the user to a tenant?', ['Yes', 'No'], 'Yes')
-            ->andReturn('Yes');
-
-        // Register our new command with the mocked ask and anticipate methods
-        app()['Illuminate\Contracts\Console\Kernel']->registerCommand($command);
-
-        $this->artisan('tenant:create-user', ['--fake' => true]);
+        $this->fireCommand('tenant:create-user', ['--fake' => true]);
 
         $this->assertContains(User::first()->email . ' with the password `tester` was created without any tenants', trim(Artisan::output()));
 
@@ -41,34 +31,18 @@ class ArtisanTest extends TestCase
 
     public function testFakeUserCanBeCreatedWithTenant()
     {
-
         // Create the tenant that we'll attach the user to
         $tenant = factory(Tenant::class)->create();
 
         // In order to properly test our commands, we need to mock the ask()
         // and anticipate() methods but leave the rest of the class to behave as normal
-        $faker = \Faker\Factory::create();
-        $command = \Mockery::mock("\MultiTenantLaravel\App\Commands\CreateUser[ask, anticipate]", [$faker])->makePartial();
+        $this->mockCommand("\MultiTenantLaravel\App\Commands\CreateUser[ask, anticipate]");
 
-        $command->shouldReceive('ask')
-            ->once()
-            ->with('How many would you like to create?')
-            ->andReturn('1');
+        $this->asks('How many would you like to create?', '1');
+        $this->asks('Please enter the id of the desired tenant.', (string) $tenant->id);
+        $this->anticipates('Would you like to assign the user to a tenant?', ['Yes', 'No'], 'Yes', 'Yes');
 
-        $command->shouldReceive('ask')
-            ->once()
-            ->with('Please enter the id of the desired tenant.')
-            ->andReturn((string) $tenant->id);
-
-        $command->shouldReceive('anticipate')
-            ->once()
-            ->with('Would you like to assign the user to a tenant?', ['Yes', 'No'], 'Yes')
-            ->andReturn('Yes');
-
-        // Register our new command with the mocked ask and anticipate methods
-        app()['Illuminate\Contracts\Console\Kernel']->registerCommand($command);
-
-        $this->artisan('tenant:create-user', ['--fake' => true]);
+        $this->fireCommand('tenant:create-user', ['--fake' => true]);
 
         $this->assertContains('The user ' . User::get()->last()->email . ' is now the owner of ' . $tenant->name . ' with the password `tester`', trim(Artisan::output()));
     }
@@ -77,22 +51,12 @@ class ArtisanTest extends TestCase
     {
         // In order to properly test our commands, we need to mock the ask()
         // and anticipate() methods but leave the rest of the class to behave as normal
-        $faker = \Faker\Factory::create();
-        $command = \Mockery::mock("\MultiTenantLaravel\App\Commands\CreateTenant[ask, anticipate]", [$faker])->makePartial();
+        $this->mockCommand("\MultiTenantLaravel\App\Commands\CreateTenant[ask, anticipate]");
 
-        $command->shouldReceive('ask')
-            ->with('How many would you like to create?')
-            ->andReturn('1');
+        $this->asks('How many would you like to create?', '1');
+        $this->anticipates('Would you like to create a new user, or use an existing?', ['New', 'Existing'], 'New', 'New');
 
-        $command->shouldReceive('anticipate')
-            ->once()
-            ->with('Would you like to create a new user, or use an existing?', ['New', 'Existing'], 'New')
-            ->andReturn('New');
-
-        // Register our new command with the mocked ask and anticipate methods
-        app()['Illuminate\Contracts\Console\Kernel']->registerCommand($command);
-
-        $this->artisan('tenant:create-tenant', ['--fake' => true]);
+        $this->fireCommand('tenant:create-tenant', ['--fake' => true]);
 
         $this->assertContains('The user ' . User::first()->email . ' is now the owner of ' . Tenant::first()->name . ' with the password', trim(Artisan::output()));
 
@@ -102,29 +66,16 @@ class ArtisanTest extends TestCase
 
     public function testFakeTenantCanBeCreatedWithExistingUser()
     {
+        $user = factory(User::class)->create();
         // In order to properly test our commands, we need to mock the ask()
         // and anticipate() methods but leave the rest of the class to behave as normal
-        $faker = \Faker\Factory::create();
-        $command = \Mockery::mock("\MultiTenantLaravel\App\Commands\CreateTenant[ask, anticipate]", [$faker])->makePartial();
+        $this->mockCommand("\MultiTenantLaravel\App\Commands\CreateTenant[ask, anticipate]");
 
-        $command->shouldReceive('ask')
-            ->with('How many would you like to create?')
-            ->andReturn('1');
+        $this->asks('How many would you like to create?', '1');
+        $this->asks('Please enter the id of the desired user.', (string) $user->id);
+        $this->anticipates('Would you like to create a new user, or use an existing?', ['New', 'Existing'], 'New', 'Existing');
 
-        $user = factory(User::class)->create();
-        $command->shouldReceive('ask')
-            ->with('Please enter the id of the desired user.')
-            ->andReturn((string) $user->id);
-
-        $command->shouldReceive('anticipate')
-            ->once()
-            ->with('Would you like to create a new user, or use an existing?', ['New', 'Existing'], 'New')
-            ->andReturn('Existing');
-
-        // Register our new command with the mocked ask and anticipate methods
-        app()['Illuminate\Contracts\Console\Kernel']->registerCommand($command);
-
-        $this->artisan('tenant:create-tenant', ['--fake' => true]);
+        $this->fireCommand('tenant:create-tenant', ['--fake' => true]);
 
         $this->assertContains('The user ' . User::first()->email . ' is now the owner of ' . Tenant::first()->name . ' with the password', trim(Artisan::output()));
 
@@ -136,37 +87,65 @@ class ArtisanTest extends TestCase
     {
         // In order to properly test our commands, we need to mock the ask()
         // and anticipate() methods but leave the rest of the class to behave as normal
-        $faker = \Faker\Factory::create();
-        $command = \Mockery::mock("\MultiTenantLaravel\App\Commands\SyncRolesPermissionsFeatures[]", [$faker])->makePartial();
+        $this->mockCommand("\MultiTenantLaravel\App\Commands\SyncRolesPermissionsFeatures[]");
 
-        // Register our new command with the mocked ask and anticipate methods
-        app()['Illuminate\Contracts\Console\Kernel']->registerCommand($command);
-
+        // Setup some features in the config on the fly
         config(['multi-tenant.features' => [
             'posts' => ['label' => 'Posts', 'model' => 'App\Post'],
             'videos' => ['label' => 'Videos', 'model' => 'App\Video'],
         ]]);
 
-        $this->artisan('tenant:sync');
+        $this->fireCommand('tenant:sync');
 
+        // Make sure we have expected roles
         $this->assertNotEmpty(config('multi-tenant.role_class')::where('label', 'Super')->first()->id);
         $this->assertNotEmpty(config('multi-tenant.role_class')::where('label', 'Owner')->first()->id);
 
+        // Make sure we have expected features
         $this->assertNotEmpty(config('multi-tenant.feature_class')::where('label', 'Posts')->first()->id);
         $this->assertNotEmpty(config('multi-tenant.feature_class')::where('label', 'Videos')->first()->id);
 
+        // Make sure we have expected permissions
         $this->assertNotEmpty(config('multi-tenant.permission_class')::where('name', 'posts_view')->first()->id);
         $this->assertNotEmpty(config('multi-tenant.permission_class')::where('name', 'posts_edit')->first()->id);
-
         $this->assertNotEmpty(config('multi-tenant.permission_class')::where('name', 'videos_view')->first()->id);
         $this->assertNotEmpty(config('multi-tenant.permission_class')::where('name', 'videos_edit')->first()->id);
 
-        $this->artisan('tenant:sync');
-
         // We don't ever want this stuff duplicating, so we should be able to run the command multiple times
         // and only ever have a count of one for each permission, role and feature
+        $this->fireCommand('tenant:sync');
+        $this->fireCommand('tenant:sync');
+
         $this->assertEquals(1, config('multi-tenant.permission_class')::where('name', 'videos_edit')->count());
         $this->assertEquals(1, config('multi-tenant.role_class')::where('name', 'owner')->count());
         $this->assertEquals(1, config('multi-tenant.feature_class')::where('name', 'posts')->count());
+    }
+
+    private function mockCommand($file)
+    {
+        $faker = \Faker\Factory::create();
+
+        $this->command = \Mockery::mock($file, [$faker])->makePartial();
+    }
+
+    private function asks($question, $answer)
+    {
+        $this->command->shouldReceive('ask')
+            ->with($question)
+            ->andReturn($answer);
+    }
+
+    private function anticipates($question, $options, $default, $answer)
+    {
+        $this->command->shouldReceive('anticipate')
+            ->with($question, $options, $default)
+            ->andReturn($answer);
+    }
+
+    private function fireCommand($command_name, $options = [])
+    {
+        app()['Illuminate\Contracts\Console\Kernel']->registerCommand($this->command);
+
+        $this->artisan($command_name, $options);
     }
 }
